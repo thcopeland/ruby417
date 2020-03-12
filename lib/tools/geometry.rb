855026879f41962cb98d011cb4f27b02bc52f870
Tom Copeland <thcopeland2001@gmail.com>
2020-03-12 18:15:39 -0400

    Remove old localization algorithm code

--- lib/tools/geometry.rb
+++ lib/tools/geometry.rb
@@ -21,30 +21,6 @@ module Ruby417
           b.is_a?(Point) && (x - b.x).abs < 0.00001 && (y - b.y).abs < 0.00001
         end
       end
-
-      class HesseLine
-        attr_accessor :rho, :theta
-
-        def initialize(rho, theta)
-          @rho, @theta = rho, theta # x*sin(theta) - y*cos(theta) = rho
-        end
-
-        def to_std
-          StandardLine.new(Math.sin(theta), -Math.cos(theta), -rho)
-        end
-      end
-
-      class StandardLine
-        attr_accessor :a, :b, :c
-
-        def initialize(a, b, c)
-          @a, @b, @c = a, b, c # a*x + b*y + c = 0
-        end
-
-        def normal_distance_to(point)
-          (a*point.x + b*point.y + c).abs/Math.sqrt(a**2 + b**2)
-        end
-      end
     end
   end
 end
--- lib/tools/tools.rb
+++ lib/tools/tools.rb
@@ -1,5 +1,2 @@
 require_relative "geometry"
-require_relative "hough"
-require_relative "adaptive_anchor_scan"
-require_relative "collinearity"
 require_relative "rectangle_detection"
