cd7bde5af6794cb5b7e064ecbc2a80083b36ae7b
Tom Copeland <thcopeland2001@gmail.com>
2020-03-12 18:08:59 -0400

    Remove old code and tests

--- spec/lib/tools/rectangle_spec.rb
+++ spec/lib/tools/rectangle_spec.rb
@@ -7,55 +7,28 @@ RSpec.describe RectangleDetection::Rectangle do
     Point = Tools::Geometry::Point
   end
 
-  describe "#point_transform" do
-    it "maps a point from rectangle coordinates to image coordinates" do
-      rect = Rectangle.new(25, 42, 8  , 8, Math::PI/4, 100, 0, 0)
+  describe "#corners" do
+    it "calculates the corners of an axis-aligned rectangle" do
+      rect = Rectangle.new(30, 30, 8, 8, 0.0, 0, 0, 0)
+      upper_left  = Point.new(rect.cx-rect.width/2, rect.cy-rect.height/2)
+      upper_right = Point.new(rect.cx+rect.width/2, rect.cy-rect.height/2)
+      lower_right = Point.new(rect.cx-rect.width/2, rect.cy+rect.height/2)
+      lower_left  = Point.new(rect.cx+rect.width/2, rect.cy+rect.height/2)
 
-      expect(rect.send(:point_transform, 0, 0)).to eq(Point.new(25, 42))
-      expect(rect.send(:point_transform, -1, 0)).to eq(Point.new(25 - 2**-0.5, 42 - 2**-0.5))
-      expect(rect.send(:point_transform, 1, 1)).to eq(Point.new(25, 42 + 2**0.5))
-    end
-  end
-
-  describe "corners" do
-    let(:rect1) { Rectangle.new(0, 0, 8*Math.sqrt(3), 8, Math::PI/8, 0, 0, 0) }
-    let(:rect2) { Rectangle.new(0, 0, 8*Math.sqrt(3), 8, Math::PI/3, 0, 0, 0) }
-    let(:half_diagonal) { Math.hypot(rect1.width, rect1.height) / 2 }
-
-    describe "#upper_left" do
-      it "returns upper left corner" do
-        expect(rect1.upper_left).to eq(Point.new(-half_diagonal*Math.cos(Math::PI/6 + rect1.orientation),
-                                                 -half_diagonal*Math.sin(Math::PI/6 + rect1.orientation)))
-        expect(rect2.upper_left).to eq(Point.new(-half_diagonal*Math.cos(Math::PI/6 - rect2.orientation),
-                                                 +half_diagonal*Math.sin(Math::PI/6 - rect2.orientation)))
-      end
+      expect(rect.corners).to match_array [upper_left, upper_right, lower_right, lower_left]
     end
 
-    describe "#upper_right" do
-      it "returns upper right corner" do
-        expect(rect1.upper_right).to eq(Point.new(+half_diagonal*Math.cos(Math::PI/6 - rect1.orientation),
-                                                  -half_diagonal*Math.sin(Math::PI/6 - rect1.orientation)))
-        expect(rect2.upper_right).to eq(Point.new(-half_diagonal*Math.cos(Math::PI/6 + rect2.orientation),
-                                                  -half_diagonal*Math.sin(Math::PI/6 + rect2.orientation)))
-      end
-    end
+    it "calculates the corners of arbitrarily rotated rectangle" do
+      rect = Rectangle.new(0, 0, 8*Math.sqrt(3), 8, Math::PI/3, 0, 0, 0)
 
-    describe "#lower_left" do
-      it "returns lower left corner" do
-        expect(rect1.lower_left).to eq(Point.new(-half_diagonal*Math.cos(Math::PI/6 - rect1.orientation),
-                                                 +half_diagonal*Math.sin(Math::PI/6 - rect1.orientation)))
-        expect(rect2.lower_left).to eq(Point.new(-half_diagonal*Math.cos(Math::PI/6 + rect2.orientation),
-                                                 +half_diagonal*Math.sin(Math::PI/6 + rect2.orientation)))
-      end
-    end
+      # Math::PI/6 is a consequence of the rectangle's dimensions, the orientation
+      # of the rectangle, however, is arbitrary.
+      upper_left  = Point.new(-8*Math.cos(Math::PI/6+rect.orientation), -8*Math.sin(Math::PI/6+rect.orientation))
+      upper_right = Point.new(+8*Math.cos(Math::PI/6-rect.orientation), -8*Math.sin(Math::PI/6-rect.orientation))
+      lower_right = Point.new(+8*Math.cos(Math::PI/6+rect.orientation), +8*Math.sin(Math::PI/6+rect.orientation))
+      lower_left  = Point.new(-8*Math.cos(Math::PI/6-rect.orientation), +8*Math.sin(Math::PI/6-rect.orientation))
 
-    describe "#lower_right" do
-      it "returns lower right corner" do
-        expect(rect1.lower_right).to eq(Point.new(+half_diagonal*Math.cos(Math::PI/6 + rect1.orientation),
-                                                  +half_diagonal*Math.sin(Math::PI/6 + rect1.orientation)))
-        expect(rect2.lower_right).to eq(Point.new(+half_diagonal*Math.cos(Math::PI/6 - rect2.orientation),
-                                                  -half_diagonal*Math.sin(Math::PI/6 - rect2.orientation)))
-      end
+      expect(rect.corners).to match_array [upper_left, upper_right, lower_right, lower_left]
     end
   end
 end
